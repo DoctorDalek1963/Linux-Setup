50e5163c171474a2af6ecd5fac24203832fe9ff7
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-01-02 17:56:35 +0000

    Update setup.sh with function calls

--- setup.sh
+++ setup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+# Regex to check user input
 yn_regex='^[Yy].*'
+
+# Function to optionally copy a file (takes one argument; returns 0 if successful and 1 if unsuccessful, and 2 if not copied)
+OptionalCopy() {
+    read -p "Would you like to copy $1 to your home directory? (Y/n) " copy_var
+
+    if [[ $copy_var =~ $yn_regex ]]
+    then
+        cp $1 ~ && echo "$1 copied";return 0 || echo "$1 failed to copy";return 1
+    else
+        echo "$1 not copied";return 2
+    fi
+}
 
 # Copy the .vimrc and .bash_aliases to the home directory
 cp .vimrc ~
@@ -10,24 +23,15 @@ echo ".bash_aliases copied"
 source ~/.bash_aliases
 echo ".bash_aliases sourced"
 
-read -p "Would you like to copy .bashrc to your home directory? (Y/n) " bashrc_copy_var
+echo
 
-if [[ $bashrc_copy_var =~ $yn_regex ]]
+# Copy .bashrc and if successful, source it
+OptionalCopy .bashrc
+if [[ $? -eq 0 ]]
 then
-    cp .bashrc ~
-    echo ".bashrc copied"
-    source ~/.bashrc
-    echo ".bashrc sourced"
-else
-    echo ".bashrc not copied"
+    source ~/.bashrc && echo ".bashrc sourced"
 fi
 
-read -p "Would you like to copy .gitconfig to your home directory (Y/n) " gtcfg_copy_var
+echo
 
-if [[ $gtcfg_copy_var =~ $yn_regex ]]
-then
-    cp .gitconfig ~
-    echo ".gitconfig copied"
-else
-    echo ".gitconfig not copied"
-fi
+OptionalCopy .gitconfig
