472384298dea257c69a4e2c73bb5621d22e801be
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-01-03 11:24:19 +0000

    Update setup.sh with OptionalCopyAndSource function and make all file copying optional

--- setup.sh
+++ setup.sh
@@ -16,8 +16,25 @@ OptionalCopy() {
     fi
 }
 
+OptionalCopyAndSource () {
+    # I just copied the whole OptionalCopy function in here because calling the function didn't seem to work
+    echo # Create a blank line to pad things out a bit
+    read -p "Would you like to copy $1 to your home directory? (Y/n) " copy_var
+
+    if [[ $copy_var =~ $yn_regex ]]
+    then
+        cp $1 ~ && echo "$1 copied";source_var=true || echo "$1 failed to copy";success_var=false
+    else
+        echo "$1 not copied";source_var=false
+    fi
+
+    if $source_var ; then
+        source ~/$1 && echo "$1 sourced" || echo "$1 failed to source"
+    fi
+}
+
 # Warn the user before doing anything
-echo "WARNING: This will overwrite your ~/.vimrc and ~/.bash_aliases files automatically, with the option to overwrite your ~/.bashrc and ~/.gitconfig files"
+echo "WARNING: This will optionally overwrite your ~/.vimrc, ~/.bash_aliases, ~/.bashrc, and ~/.gitconfig files"
 read -p "Would you like to continue? (Y/n) " continue_var
 
 if [[ ! $continue_var =~ $yn_regex ]] # If not continue, quit script
@@ -25,23 +42,10 @@ then
     exit 0 # Exit successfully
 fi
 
-echo
+OptionalCopy .vimrc
 
-# Copy the .vimrc and .bash_aliases to the home directory
-cp .vimrc ~
-echo ".vimrc copied"
-cp .bash_aliases ~
-echo ".bash_aliases copied"
-source ~/.bash_aliases
-echo ".bash_aliases sourced"
+OptionalCopyAndSource .bash_aliases
 
-# Copy .bashrc and if successful, source it
-OptionalCopy .bashrc
+OptionalCopyAndSource .bashrc
 
-if [[ $? -eq 0 ]]
-then
-    source ~/.bashrc && echo ".bashrc sourced"
-fi
-
-# Copy .gitconfig
 OptionalCopy .gitconfig
