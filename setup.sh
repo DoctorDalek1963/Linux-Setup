61826835ffa40c580dbf82839fd8efb0d36b4987
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2021-01-06 17:04:00 +0000

    Add options for install vim plugin stuff in setup.sh and make it prettier

--- setup.sh
+++ setup.sh
@@ -33,14 +33,19 @@ OptionalCopyAndSource () {
     fi
 }
 
+########## USER INTERACTION ##########
+
 # Warn the user before doing anything
-echo "WARNING: This will optionally overwrite your ~/.vimrc, ~/.bash_aliases, ~/.bashrc, and ~/.gitconfig files"
+echo "WARNING: This script has options to overwrite important files and install lots of stuff, which will take time and bandwidth"
 read -p "Would you like to continue? (Y/n) " continue_var
 
 if [[ ! $continue_var =~ $yn_regex ]] # If not continue, quit script
 then
     exit 0 # Exit successfully
 fi
+
+echo
+echo "========== FILE MOVEMENT =========="
 
 OptionalCopy .vimrc
 
@@ -49,3 +54,57 @@ OptionalCopyAndSource .bash_aliases
 OptionalCopyAndSource .bashrc
 
 OptionalCopy .gitconfig
+
+echo
+echo "========== VIM PLUGINS =========="
+echo
+echo "NOTE: If you don't install vim-plug and Vundle but you do copy .vimrc, the plugins won't load"
+echo
+
+read -p "Would you like to install vim-plug? (Y/n) " vim_plug_var
+
+if [[ $vim_plug_var =~ $yn_regex ]]
+then
+    # Install vim-plug in autoload directory
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+echo
+
+read -p "Would you like to install and update all vim-plug plugins? (Y/n) " vim_plug_install_var
+
+if [[ $vim_plug_install_var =~ $yn_regex ]]
+then
+    vim -c "PlugInstall" -c "qa!"
+    vim -c "PlugUpdate" -c "qa!"
+fi
+echo
+
+read -p "Would you like to install Vundle? (Y/n) " vundle_var
+
+if [[ $vundle_var =~ $yn_regex ]]
+then
+    # Clone github repo for Vundle into proper directory
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+echo
+
+read -p "Would you like to install all depenedencies for the YouCompleteMe plugin (this will take a long time)? (Y/n) " ycm_var
+
+if [[ $ycm_var =~ $yn_regex ]]
+then
+    sudo apt -y install build-essential cmake vim-nox python3-dev
+    sudo apt -y install mono-complete golang nodejs default-jdk npm
+    python3 ~/.vim/bundle/YouCompleteMe/install.py --all
+fi
+echo
+
+read -p "Would you like to install and update all Vundle plugins? (Y/n) " vundle_install_var
+
+if [[ $vundle_install_var =~ $yn_regex ]]
+then
+    vim -c "PluginInstall" -c "qa!"
+    vim -c "PluginUpdate" -c "qa!"
+fi
+echo
+
+echo "Thanks for using this setup utility!"
