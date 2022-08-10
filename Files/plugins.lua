499bb5952ac048f3a28ce8f368453d787d03b5e6
DoctorDalek1963 <69600500+DoctorDalek1963@users.noreply.github.com>
2022-08-10 17:41:44 +0100

    Allow packer.nvim to bootstrap itself

--- Files/plugins.lua
+++ Files/plugins.lua
@@ -1,3 +1,11 @@
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
+end
+
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 
@@ -41,4 +49,8 @@ return require('packer').startup(function(use)
 	use 'itchyny/lightline.vim'
 	use 'maximbaz/lightline-ale'
 	use 'josa42/vim-lightline-coc'
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 end)
