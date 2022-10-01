6c59e85036844d1c80771836ee442d54b8df9447
DoctorDalek1963 <dyson.dyson@icloud.com>
2022-10-01 21:37:55 +0100

    Fix E592 by allowing zero-width windows

--- Files/plugins.lua
+++ Files/plugins.lua
@@ -304,8 +304,8 @@ return require('packer').startup(function(use)
 			'anuvyklack/animation.nvim'
 		},
 		config = function()
-			vim.o.winwidth = 10
-			vim.o.winminwidth = 10
+			vim.o.winwidth = 15
+			vim.o.winminwidth = 0
 			vim.o.equalalways = false
 			require('windows').setup()
 		end
